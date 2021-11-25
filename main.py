153c81c961583026abcd5aad4ec5d5be1438d915
Lr-2002 <2629651228@qq.com>
2021-11-25 11:04:17 +0800

    done

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+import shutil
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def get_file_size(file_path):
+    fsize = os.path.getsize(file_path)
+    return fsize
 
 
-# Press the green button in the gutter to run the script.
+def cp_file(path, file):
+    file_name = os.path.join(path, file)
+    shutil.copyfile(file, file_name)
+    return file_name
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    path = os.getcwd()
+    file_dir = os.path.join(path,'test_file1')
+    if not os.path.isdir(file_dir):
+        os.mkdir(os.path.join(path,'test_file1'))
+    file = './main.py'
+    cp_file(file_dir, './main.py')
+    print("file_size:",get_file_size(file))
+    write_file = os.path.join(file_dir, 'reversed_file.py')
+    with open(file,'r') as f:
+        content = f.read()
+        reversed_content = []
+        with open(write_file , 'a') as w:
+            for i in range(0, len(content)):
+                w.write(content[len(content)-1-i])
+    file_list = os.listdir('./')
+    file_count = 0
+    file_size_count = 0
+    for i in file_list:
+        if os.path.isdir(i):
+            continue
+        else:
+            file_count += 1
+            size = get_file_size(cp_file(file_dir, i))
+            file_size_count += size
+            print(i, size)
+    print("total files :", file_count)
+    print("total size  :", file_size_count)
